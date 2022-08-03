475575ff2472f5c49a8fa1f22e633dfde0b7bff7
ellard <ellard@exapps.nl>
2022-08-03 23:05:13 +0200

    Removed host 0.0.0.0 and base ddev hostname from vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ export default defineConfig(({ command, mode }) => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env = loadEnv(mode, process.cwd(), '')
-    console.log(mode)
-    console.log(env.DDEV_HOSTNAME)
     return {
-        server: {
-          host: '0.0.0.0',
-            base: env.DDEV_HOSTNAME,
-        },
+
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.js'],
